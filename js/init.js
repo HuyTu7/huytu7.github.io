c80dde0bc3ac5a8a450deef99af200c5cfca2d5d
Ken Tu <huyqtu7@gmail.com>
2020-02-24 11:33:13 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -25,9 +25,8 @@
 	    var target = this.hash,
 	    $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
+	    $('html, body').stop().animate(
+		800, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
